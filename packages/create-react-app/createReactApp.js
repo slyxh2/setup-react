543059b5977336aed9ca582274115992a87b533f
slyxh2 <750772121@qq.com>
2022-08-25 13:18:14 -0400

    finish creating react app

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -5,9 +5,8 @@ const { Command } = require('commander');
 const chalk = require('chalk');
 // import chalk from 'chalk';
 const packageJSON = require('./package.json');
-const { fstat } = require('fs-extra');
-const { runMain } = require('module');
 
+const spawn = require('cross-spawn');
 
 async function init() {
     let projectName;
@@ -44,7 +43,43 @@ async function creatApp(name) {
 }
 
 async function run(root, name, originalDirectory) {
-
+    let scriptName = 'react-scripts';
+    let templateName = 'cra-template-redux'; //模板名字在这！！！
+    const allDependencies = ['react', 'react-dom', scriptName, templateName];
+    console.log('Installing packages. This might take a couple of minutes');
+    console.log(
+        `Installing ${chalk.cyan('react')}, ${chalk.cyan(
+            'react-dom'
+        )}, and ${chalk.cyan(scriptName)}${` with ${chalk.cyan(templateName)}`}...`
+    );
+    await install(root, allDependencies);
+    let data = [root, name, true, originalDirectory, templateName];
+    let source = `
+    const init = require('react-scripts/scripts/init.js');
+    init.apply(null, JSON.parse(process.argv[1]));
+    `;
+    await executeNodeScript({ cwd: process.cwd() }, data, source);
+    console.log('Done!');
+    process.exit(0);
+}
+async function executeNodeScript({ cwd }, data, source) {
+    return new Promise((resolve) => {
+        const child = spawn(
+            process.execPath,
+            ['-e', source, '--', JSON.stringify(data)],
+            { cwd, stdio: 'inherit' }
+        );
+        child.on('close', resolve);
+    });
+}
+async function install(root, allDependencies) {
+    return new Promise((resolve) => {
+        const command = 'yarnpkg';
+        const args = ['add', '--exact', ...allDependencies, '--cwd', root];
+        console.log(command, args);
+        const child = spawn(command, args, { stdio: 'inherit' });
+        child.on('close', resolve);
+    });
 }
 
 
